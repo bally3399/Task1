4134a728083ff78c16c80d16f0c13358a1204dc2
bally3399 <151347697+bally3399@users.noreply.github.com>
2023-11-29 00:38:41 +0100

    Add files via upload

---
+++ HigestScore.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class HighestScore{
+
+	public static void main(String[] args){
+
+	Scanner input = new Scanner(System.in);
+
+	int highestScore = 0;
+	String highestStudentName = " ";
+
+	System.out.print("Enter number of student");
+	int numberOfStudent = input.nextInt();
+
+	System.out.print("Enter each student's name and score");
+
+	for(int input = 0 , input < numberOfStudent, Input ++);
+
+		System.out.print("student : " + (input + input) + \n  Name: ")
+		String name = input.nextLine();
+
+		System.out.print("score: ");
+		int score = input.nextInt();
+
+	
+	}
+
+
+
+
+
+}
